9242928acdf73d12f1eced260af1884cc9700fe5
Lucia <lucy_17_4@hotmail.com>
2022-05-10 14:34:11 -0300

    Add aseguradoras screen

---
+++ src/sections/aseguradoras/aseguradoras.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./aseguradoras.scss";
+
+const Aseguradoras = () => {
+    return(
+        <div className="aseguradoras">
+            
+        </div>
+    );
+}
+
+export default Aseguradoras;
